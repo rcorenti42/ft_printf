9dc967eefd02e9c06e87b0bae48fec2f38811401
Richard CORENTIN <rcorenti@e1r9p17.42.fr>
2020-02-14 02:42:09 +0100

    protect strlen

--- libft/srcs/ft_strlen.c
+++ libft/srcs/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: rcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 23:28:14 by rcorenti          #+#    #+#             */
-/*   Updated: 2019/10/15 23:28:29 by rcorenti         ###   ########.fr       */
+/*   Updated: 2020/02/14 02:39:54 by rcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 		i++;
 	return (i);
